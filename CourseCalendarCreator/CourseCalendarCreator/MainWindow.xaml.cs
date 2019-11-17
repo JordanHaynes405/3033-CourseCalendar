19c91334cb32946fbb23d6869cc2a92b32a8e572
Jordan Haynes <jordan.t.haynes-1@ou.edu>
2019-11-17 17:07:50 -0600

    Messages for user ease

--- CourseCalendarCreator/CourseCalendarCreator/MainWindow.xaml.cs
+++ CourseCalendarCreator/CourseCalendarCreator/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -50,73 +51,30 @@ namespace CourseCalendarCreator
             {
                 MessageBox.Show("You must set up the calendar before exporting to Excel!");
             }
-            else if (Topics.Count == 0 )
+            //else if (Topics.Count == 0 )
+            //{
+            //    MessageBox.Show("You have not entered any topics for the course calendar");
+            //}
+            else if (Topics.Count < 3 )
             {
-                MessageBox.Show("You have not entered any topics for the course calendar");
+                DialogResult dialogResult = MessageBox.Show("Sure", "Some Title", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    //do something
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    //do something else
+                }
+                MessageBoxButton.YesNo.("You don't have very many topics in your course calendar, consider adding more!");
             }
-            else if (Topics.Count == 1 )
+            else if (txtTopicName.Text != null && txtNumTopicPeriods.Text != null && txtTopicPreparation.Text != null && pcrTopicStart.Text != null)
             {
-                MessageBox.Show("You don't have very many topics in your course calendar, consider adding more!");
+                MessageBox.Show("You should click the ADD button to save your last entry before exporting!");
             }
             else
             {
-                using (ExcelPackage Excel = new ExcelPackage())
-                {
-                    var CalendarSheet = Excel.Workbook.Worksheets.Add("Worksheet1");
-                    Microsoft.Win32.SaveFileDialog DialogT = new Microsoft.Win32.SaveFileDialog();
-                    var File = DialogT.ShowDialog();
-                    string Path = DialogT.FileName;
-                    FileInfo ExcelFile = new FileInfo(Path + ".xlsx");
-
-                    CalendarSheet.Cells["A1"].Value = ($"Course Name: {txtCourseName.Text}");
-                    CalendarSheet.Cells["A2"].Value = ($"Course Code: {txtCourseCode.Text}");
-                    CalendarSheet.Cells["A3"].Value = ($"Professor {txtProfessor.Text}");
-                    CalendarSheet.Cells["A4"].Value = ($"Semester {txtSemester.Text}");
-                    CalendarSheet.Cells["C4"].Value = ($"Day(s): {txtDaysofWeek.Text}");
-                    CalendarSheet.Cells["A5"].Value = ($"Start Date: {dprStart.Text}");
-                    CalendarSheet.Cells["C5"].Value = ($"End Date: {dprEnd.Text}");
-
-                    //Build Columns
-                    CalendarSheet.Cells["A6"].Value = "Date";
-                    CalendarSheet.Cells["B6"].Value = "Topic";
-                    CalendarSheet.Cells["C6"].Value = "Classes";
-                    CalendarSheet.Cells["D6"].Value = "Preparation";
-
-                    for (int i = 0; i <= ItemsAdded - 1; i++)
-                    {
-                        CalendarSheet.Cells[$"A{7 + i}"].Value = TopicDate[i].ToShortDateString();
-                        CalendarSheet.Cells[$"B{7 + i}"].Value = Topics[i];
-                        CalendarSheet.Cells[$"C{7 + i}"].Value = Convert.ToInt32(Periods[i]);
-                        CalendarSheet.Cells[$"D{7 + i}"].Value = Preparations[i];
-                    }
-
-                    var Heading = CalendarSheet.Cells["A1:D6"];
-                    Heading.Style.Font.Bold = true;
-                    Heading.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-
-                    var Body = CalendarSheet.Cells[$"A6:D{ItemsAdded + 7}"];
-                    Body.Style.Border.Top.Style = Body.Style.Border.Left.Style = 
-                        Body.Style.Border.Right.Style = Body.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
-
-                    var Whole = CalendarSheet.Cells[$"A1:D{ItemsAdded + 7}"];
-                    Whole.Style.WrapText = true;
-
-
-                    CalendarSheet.Column(1).Width = 15;
-                    CalendarSheet.Column(2).Width = 26;
-                    CalendarSheet.Column(3).Width = 12;
-                    CalendarSheet.Column(4).Width = 34;
-
-                    CalendarSheet.Cells["A1:D1"].Merge = true;
-                    CalendarSheet.Cells["A2:D2"].Merge = true;
-                    CalendarSheet.Cells["A3:D3"].Merge = true;
-                    CalendarSheet.Cells["A4:B4"].Merge = true;
-                    CalendarSheet.Cells["C4:D4"].Merge = true;
-                    CalendarSheet.Cells["A5:B5"].Merge = true;
-                    CalendarSheet.Cells["C5:D5"].Merge = true;
-
-                    Excel.SaveAs(ExcelFile);
-                }
+                BuildSpreadSheet();
             }
         }
         private void btnDelete_Click(object sender, RoutedEventArgs e)
@@ -154,18 +112,88 @@ namespace CourseCalendarCreator
         }
         public void AddToTable()
         {
-            //CourseTable.Columns.Add(new DataColumn("Date", Type.GetType("System.DateTime")));
-            //CourseTable.Columns.Add(new DataColumn("Topic Name", Type.GetType("System.String")));
-            //CourseTable.Columns.Add(new DataColumn("Periods", Type.GetType("System.String")));
-            //CourseTable.Columns.Add(new DataColumn("Preparation", Type.GetType("System.String"))); 
-            //CourseTable.Rows.Add(txtTopicName.Text, txtNumTopicPeriods.Text, txtTopicPreparation.Text);//pcrTopicStart.Text,
-            //dgPreview.ItemsSource = MainPageInput.CourseTable.DefaultView;
+            DataGridTextColumn DateCol = new DataGridTextColumn();
+            DateCol.Header = "Start Dates";
+            DateCol.Binding = new Binding(pcrTopicStart.Text);
+
+            DataGridTextColumn TopicCol = new DataGridTextColumn();
+            TopicCol.Header = "Topics";
+            TopicCol.Binding = new Binding(txtTopicName.Text);
+
+            DataGridTextColumn PeriodCol = new DataGridTextColumn();
+            PeriodCol.Header = "Periods";
+            PeriodCol.Binding = new Binding(txtNumTopicPeriods.Text);
+
+            DataGridTextColumn PreparationCol = new DataGridTextColumn();
+            PreparationCol.Header = "Preparations";
+            PreparationCol.Binding = new Binding(txtTopicPreparation.Text);
         }
 
         private void btnCloseApp_Click(object sender, RoutedEventArgs e)
         {
             //https://stackoverflow.com/questions/12977924/how-to-properly-exit-a-c-sharp-application
             Environment.Exit(0);
+        }
+        public void BuildSpreadSheet ()
+        {
+            using (ExcelPackage Excel = new ExcelPackage())
+            {
+                var CalendarSheet = Excel.Workbook.Worksheets.Add("Worksheet1");
+                Microsoft.Win32.SaveFileDialog DialogT = new Microsoft.Win32.SaveFileDialog();
+                var File = DialogT.ShowDialog();
+                string Path = DialogT.FileName;
+                FileInfo ExcelFile = new FileInfo(Path + ".xlsx");
+
+                CalendarSheet.Cells["A1"].Value = ($"Course Name: {txtCourseName.Text}");
+                CalendarSheet.Cells["A2"].Value = ($"Course Code: {txtCourseCode.Text}");
+                CalendarSheet.Cells["A3"].Value = ($"Professor {txtProfessor.Text}");
+                CalendarSheet.Cells["A4"].Value = ($"Semester {txtSemester.Text}");
+                CalendarSheet.Cells["C4"].Value = ($"Day(s): {txtDaysofWeek.Text}");
+                CalendarSheet.Cells["A5"].Value = ($"Start Date: {dprStart.Text}");
+                CalendarSheet.Cells["C5"].Value = ($"End Date: {dprEnd.Text}");
+
+                //Build Columns
+                CalendarSheet.Cells["A6"].Value = "Date";
+                CalendarSheet.Cells["B6"].Value = "Topic";
+                CalendarSheet.Cells["C6"].Value = "Classes";
+                CalendarSheet.Cells["D6"].Value = "Preparation";
+
+                int ExcelIterate = ItemsAdded;
+                for (int i = 0; i <= ExcelIterate - 1; i++)
+                {
+                    CalendarSheet.Cells[$"A{7 + i}"].Value = TopicDate[i].ToShortDateString();
+                    CalendarSheet.Cells[$"B{7 + i}"].Value = Topics[i];
+                    CalendarSheet.Cells[$"C{7 + i}"].Value = Convert.ToInt32(Periods[i]);
+                    CalendarSheet.Cells[$"D{7 + i}"].Value = Preparations[i];
+                }
+
+                var Heading = CalendarSheet.Cells["A1:D6"];
+                Heading.Style.Font.Bold = true;
+                Heading.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                var Body = CalendarSheet.Cells[$"A6:D{ItemsAdded + 7}"];
+                Body.Style.Border.Top.Style = Body.Style.Border.Left.Style =
+                    Body.Style.Border.Right.Style = Body.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+
+                var Whole = CalendarSheet.Cells[$"A1:D{ItemsAdded + 7}"];
+                Whole.Style.WrapText = true;
+
+
+                CalendarSheet.Column(1).Width = 15;
+                CalendarSheet.Column(2).Width = 26;
+                CalendarSheet.Column(3).Width = 12;
+                CalendarSheet.Column(4).Width = 34;
+
+                CalendarSheet.Cells["A1:D1"].Merge = true;
+                CalendarSheet.Cells["A2:D2"].Merge = true;
+                CalendarSheet.Cells["A3:D3"].Merge = true;
+                CalendarSheet.Cells["A4:B4"].Merge = true;
+                CalendarSheet.Cells["C4:D4"].Merge = true;
+                CalendarSheet.Cells["A5:B5"].Merge = true;
+                CalendarSheet.Cells["C5:D5"].Merge = true;
+
+                Excel.SaveAs(ExcelFile);
+            }
         }
     }
 }
