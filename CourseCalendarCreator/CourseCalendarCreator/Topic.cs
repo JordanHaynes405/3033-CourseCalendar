c6f795c1f25d6a48ef917bb6e0d0bbfa83ed9a4e
Jordan Haynes <jordan.t.haynes-1@ou.edu>
2019-11-18 15:27:49 -0600

    Created a Topic Class

---
+++ CourseCalendarCreator/CourseCalendarCreator/Topic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCalendarCreator
+{
+    class Topic
+    {
+        public DateTime TopicStartDate { get; set; }
+        public string TopicName { get; set; }
+        public int TopicPeriods { get; set; }
+        public string Preparatinon { get; set; }
+    }
+}
